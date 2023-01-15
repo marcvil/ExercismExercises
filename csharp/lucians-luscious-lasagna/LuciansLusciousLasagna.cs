a6c6dde24e75c4f29fc4d49087e529d35d1e2cf7
Marc Vilà <marcvilaespunya@gmail.com>
2023-01-15 13:10:38 +0100

    2 exercises

--- csharp/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
+++ csharp/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
@@ -1,10 +1,26 @@
 class Lasagna
 {
     // TODO: define the 'ExpectedMinutesInOven()' method
+    public int ExpectedMinutesInOven()
+    {
+        return 40;
+    }
 
     // TODO: define the 'RemainingMinutesInOven()' method
+    public int RemainingMinutesInOven(int remaining)
+    {
+        return ExpectedMinutesInOven()- remaining;
+    }
 
     // TODO: define the 'PreparationTimeInMinutes()' method
+    public int PreparationTimeInMinutes(int layers)
+    {
+        return layers * 2;
+    }
 
     // TODO: define the 'ElapsedTimeInMinutes()' method
+    public int ElapsedTimeInMinutes(int layers,int remianing)
+    {
+        return PreparationTimeInMinutes(layers) + remianing;
+    }
 }
